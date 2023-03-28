28974daac478e08f6b4494c15ab7fdd1063bbbb7
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-03-28 11:29:03 -0300

    updated GlobalStyle

--- src/assets/styles/GlobalStyle.style.tsx
+++ src/assets/styles/GlobalStyle.style.tsx
@@ -1,3 +1,15 @@
 import { createGlobalStyle } from 'styled-components';
 
-export const GlobalStyle = createGlobalStyle``;
+export const GlobalStyle = createGlobalStyle`
+   @import url('https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap');
+
+   *{
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+   }
+
+   body{
+      font-family: 'DM Sans', sans-serif;
+   }
+`;
