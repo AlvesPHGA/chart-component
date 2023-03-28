3ad7e507d25c038cce54277e8c650267c7b293e2
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-03-28 11:33:18 -0300

    add a class in ClassUtils style

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -4,14 +4,15 @@ import { Index as Header } from '../components/global/Header/index';
 import { Index as Footer } from '../components/global/Footer/index';
 
 import { Index as Main } from '../components/Main/index';
+import { Container } from '../assets/styles/ClassUtils.style';
 
 const Home = () => {
    return (
-      <>
+      <Container>
          <Header />
          <Main />
          <Footer />
-      </>
+      </Container>
    );
 };
 
