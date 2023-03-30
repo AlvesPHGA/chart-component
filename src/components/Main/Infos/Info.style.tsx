d2099897ba92e29f819c3f64b8b9d7c4c1e73467
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-03-30 17:19:29 -0300

    was created a new component to Info div

---
+++ src/components/Main/Infos/Info.style.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const Info = styled.div`
+   display: flex;
+   align-items: center;
+   justify-content: space-between;
+
+   & .total {
+      & h2 {
+         margin-top: ${4 / 16}rem;
+         font-size: ${30 / 16}rem;
+      }
+   }
+
+   & .percents {
+      text-align: right;
+
+      & span {
+         display: block;
+         font-weight: 700;
+
+         &:nth-child(2) {
+            margin-top: ${4 / 16}rem;
+            font-weight: 400;
+         }
+      }
+   }
+`;
