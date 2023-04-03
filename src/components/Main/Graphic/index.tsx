34447928ecf8066cc65e840331bed5ff8f0d7154
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-04-03 17:39:15 -0300

    updated (index) Graphic component

--- src/components/Main/Graphic/index.tsx
+++ src/components/Main/Graphic/index.tsx
@@ -21,37 +21,11 @@ export const Index = () => {
       getData();
    }, []);
 
-   console.log(data);
-
    return (
       <Graphic>
          {data.map((data) => (
             <Column key={data.day} {...data} />
          ))}
-         {/* <div className="graphic__item">
-            <div className="item item--active">{}</div>
-            <span className="sub__legend mediumBrown">tue</span>
-         </div>
-         <div className="graphic__item">
-            <div className="item"></div>
-            <span className="sub__legend mediumBrown">wed</span>
-         </div>
-         <div className="graphic__item">
-            <div className="item"></div>
-            <span className="sub__legend mediumBrown">thu</span>
-         </div>
-         <div className="graphic__item">
-            <div className="item"></div>
-            <span className="sub__legend mediumBrown">fri</span>
-         </div>
-         <div className="graphic__item">
-            <div className="item"></div>
-            <span className="sub__legend mediumBrown">sat</span>
-         </div>
-         <div className="graphic__item">
-            <div className="item"></div>
-            <span className="sub__legend mediumBrown">sun</span>
-         </div> */}
       </Graphic>
    );
 };
