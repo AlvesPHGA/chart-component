1d76ff87b21e74543acc08e87fa2f3b4d7af1ab3
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-04-05 11:26:11 -0300

    add head component in Home

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -5,10 +5,15 @@ import { Index as Footer } from '../components/global/Footer/index';
 
 import { Index as Main } from '../components/Main/index';
 import { Container } from '../assets/styles/ClassUtils.style';
+import { Head } from '../components/global/Head/Head';
 
 const Home = () => {
    return (
       <Container>
+         <Head
+            title="Personal Balance"
+            description="Personal Balance on week"
+         />
          <div className="card">
             <Header />
             <Main />
